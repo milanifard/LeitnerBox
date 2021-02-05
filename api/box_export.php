c6d4c2da65781bd60fee6f165b73de3378f0afa6
Parsa <salehipour.edu@gmail.com>
2021-02-05 16:06:29 +0330

    trying to get export working

--- api/box_export.php
+++ api/box_export.php
@@ -5,26 +5,31 @@ include_once 'section.php';
 include_once 'card.php';
 
 
-function export_box($box_id , $conn  , $person_id){
+function export_box($box_id, $conn, $person_id)
+{
     $date_str = date("Y_m_d_G_i");
-
+    $user_dir =  BASE_PATH . 'user_files/boxes/' . $person_id;
+    if (!file_exists($user_dir)) {
+        mkdir($user_dir, 0777, true);
+    }
     $section = new Section($conn);
     $box_sections = $section->readByBoxId(100, $box_id);
-    $fp = fopen('../user_files/boxes/'.$person_id.'/section_'.$box_id.'_'.$date_str.'_.json', 'w');
+    $file_name = $user_dir . '/sections_' . $box_id . '_' . $date_str . '_.json';
+    $fp = fopen($file_name, 'w');
     fwrite($fp, json_encode($box_sections));
     fclose($fp);
     foreach ($box_sections as $current_section) {
         $card = new Card($conn);
         $section_cards = $card->readBySectionId(100,  $current_section['id']);
-        $fp = fopen('../user_files/boxes/'.$person_id.'/cards_'.$box_id.'_'.$date_str.'_.json', 'w');
-        fwrite($fp, json_encode($section_cards));
+        $file_name = $user_dir . '/cards_' . $box_id . '_' . $date_str . '_.txt';
+          
+
+
+        $sections_var = var_export($section_cards,true);
+        $sections_var =  mb_convert_encoding($sections_var, 'UTF-8', 'auto');
+        $fp = fopen($file_name, 'w');
+        fwrite($fp, $sections_var);
         fclose($fp);
-
-        foreach ($section_cards as $current_card) {
-            var_dump($current_card);
-        }
+        var_dump($sections_var);
     }
-
 }
-
-?>
